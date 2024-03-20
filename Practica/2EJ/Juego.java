49ec8795232442337fa323ef56aed068ddfdc68e
milena confeggi <128049813+MilenaConfeggi@users.noreply.github.com>
2024-03-20 12:40:49 -0300

    Create Juego.java

---
+++ Practica/2EJ/Juego.java
@@ -0,0 +1,12 @@
+package ar.edu.unlp.info.oo2.ej2;
+
+public class Juego {
+	
+	
+	public Juego() {
+	}
+
+	public String partida(Jugada opcion1, Jugada opcion2) {
+		return opcion1.partida(opcion2);
+	}
+}
