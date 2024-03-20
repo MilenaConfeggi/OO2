ab02ffd44250144c0685f870dc7e615860e1fa36
milena confeggi <128049813+MilenaConfeggi@users.noreply.github.com>
2024-03-20 10:56:42 -0300

    Create Tweet.java

---
+++ Practica/1EJ/Tweet.java
@@ -0,0 +1,19 @@
+package ar.edu.unlp.info.oo2.proyecto_ejemplo;
+
+public class Tweet {
+	private String texto;
+
+	public Tweet(String texto) {
+		this.texto = texto;
+	}
+	
+	public void eliminar() {
+		this.texto.equals("Contenido no disponible");
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+	
+	
+}
