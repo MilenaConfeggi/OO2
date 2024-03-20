b17edc475f3be5667676c4f5f190a45a2036a700
milena confeggi <128049813+MilenaConfeggi@users.noreply.github.com>
2024-03-20 12:39:29 -0300

    Create Jugada.java

---
+++ Practica/2EJ/Jugada.java
@@ -0,0 +1,17 @@
+package ar.edu.unlp.info.oo2.ej2;
+
+public interface Jugada {
+	
+	public String partida(Jugada jugada);
+	
+	public String contraPiedra();
+	
+	public String contraPapel();
+	
+	public String contraTijera();
+	
+	public String contraLagarto();
+	
+	public String contraSpock();
+	
+}
