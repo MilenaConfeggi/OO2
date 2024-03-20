b887dee5cf4071d2085d488b2ed4f2245e9294d9
milena confeggi <128049813+MilenaConfeggi@users.noreply.github.com>
2024-03-20 10:57:08 -0300

    Create ReTweet.java

---
+++ Practica/1EJ/ReTweet.java
@@ -0,0 +1,15 @@
+package ar.edu.unlp.info.oo2.proyecto_ejemplo;
+
+public class ReTweet {
+	private Tweet original;
+
+	public ReTweet(Tweet original) {
+		this.original = original;
+	}
+
+	public Tweet getOriginal() {
+		return original;
+	}
+	
+	
+}
